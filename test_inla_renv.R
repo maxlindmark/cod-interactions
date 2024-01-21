7f3cd6ffe38acb370bf8a3b7bf1b38d1c64d5173
Max Lindmark <max.lindmark@slu.se>
2024-01-21 13:56:32 +0100

    remove test scripts for renv

--- test_inla_renv.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(sdmTMB)
-
-home <- here::here()
-d <- read_csv(paste0(home, "/data/clean/aggregated_stomach_data.csv"))
-
-mesh <- make_mesh(d, xy_cols = c("X", "Y"), cutoff = 15)
-
-plot(mesh)
